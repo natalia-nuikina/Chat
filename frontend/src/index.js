2a187ce868c88f0766355df0317861e142147387
Наталья Нуйкина <me@nuikina.ru>
2024-09-03 13:46:46 +0300

    Step 11

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,30 +1,27 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
+import { Provider as StoreProvider } from 'react-redux';
 import './index.css';
 import './i18next.js';
-import { ErrorBoundary } from '@rollbar/react';
+import { Provider, ErrorBoundary } from '@rollbar/react';
+
 import App from './App';
 import store from './slices/index.js';
 
 const rollbarConfig = {
-  accessToken: '74bc1b3567f84fce8d30f0ccaff463f6',
+  accessToken: '666fc536cb1c4f0786f0d25aad8946d4',
   environment: 'production',
 };
 
-function TestError() {
-  const a = null;
-  return a.hello();
-}
-
 const root = ReactDOM.createRoot(document.querySelector('div.h-100'));
 root.render(
-  <Provider config={rollbarConfig} store={store}>
-    <ErrorBoundary>
-      <App />
-      <TestError />
-    </ErrorBoundary>
+  <Provider config={rollbarConfig}>
+    <StoreProvider store={store}>
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
+    </StoreProvider>
   </Provider>,
 );
 
