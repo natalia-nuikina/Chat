f764f586b023173ad688c9974ec63f5054a51fde
Наталья Нуйкина <me@nuikina.ru>
2024-09-23 11:38:02 +0300

    Проверка 2

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,24 +3,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider as StoreProvider } from 'react-redux';
 import './i18next.js';
-// import { Provider, ErrorBoundary } from '@rollbar/react';
-import { ErrorBoundary } from '@rollbar/react';
+import { Provider, ErrorBoundary } from '@rollbar/react';
 
 import App from './App';
 import store from './slices/index.js';
 
-// const rollbarConfig = {
-//   accessToken: process.env.REACT_APP_ROLLBAR_TOKEN,
-//   environment: 'production',
-// };
+const rollbarConfig = {
+  accessToken: process.env.REACT_APP_ROLLBAR_TOKEN,
+  environment: 'production',
+};
 
 const root = ReactDOM.createRoot(document.querySelector('div.h-100'));
 root.render(
-  // <Provider config={rollbarConfig}>
-  <StoreProvider store={store}>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
-  </StoreProvider>,
-  // </Provider>,
+  <Provider config={rollbarConfig}>
+    <StoreProvider store={store}>
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
+    </StoreProvider>
+  </Provider>,
 );
